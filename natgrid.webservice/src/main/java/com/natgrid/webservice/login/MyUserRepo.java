c357dfdab55a295d56f39ca3c503a1e57d65459c
soumen5496 <pal.soumen@cris.org.in>
2021-01-03 21:10:26 +0530

    second commit

--- natgrid.webservice/src/main/java/com/natgrid/webservice/login/MyUserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.natgrid.webservice.login;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MyUserRepo extends JpaRepository<MyUser, Long> {
-
-	MyUser findByusername(String uname);
-}
